eef84dba42bf4bc04e82da39b460707d7084158b
SinMinKyung <msin37956@gmail.com>
2023-05-12 11:50:57 +0900

    싱크맞추기

--- src/main/java/dao/GonguDAO.java
+++ src/main/java/dao/GonguDAO.java
@@ -322,6 +322,88 @@ public class GonguDAO {
 		return resultCount;
 	}
 
+	public ArrayList<Gongu> getPopularList() {
+		ArrayList<Gongu> popularList = new ArrayList<Gongu>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = con.prepareStatement("select * from gongu order by gongu_view_count desc limit 4");
+			System.out.println(pstmt);
+			rs = pstmt.executeQuery();	
+			
+			if (rs.next()) {
+				do {
+					popularList.add(new Gongu(Integer.parseInt(rs.getString("gongu_id")),rs.getString("category_name"),rs.getString("seller_id"),rs.getString("gongu_name"),
+							rs.getString("gongu_price"),rs.getString("gongu_discount_price"),rs.getString("gongu_status"),rs.getString("gongu_startdate"),
+							rs.getString("gongu_findate"),rs.getString("gongu_reserve"),rs.getString("gongu_min"),rs.getString("thumbnail_img")));
+				} while (rs.next());
+			}	
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return popularList;
+	}
+
+	public ArrayList<Gongu> getNewList() {
+		ArrayList<Gongu> newList = new ArrayList<Gongu>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = con.prepareStatement("select * from gongu where gongu_startdate < date_format(date_add(now(), interval 7 day),"
+					+ "'%y-%m-%d') and gongu_startdate>date_format(date_sub(now(),interval 7 day),'%y-%m-%d') order by gongu_startdate asc limit 4;");
+			
+			rs = pstmt.executeQuery();	
+			
+			if (rs.next()) {
+				do {
+					newList.add(new Gongu(Integer.parseInt(rs.getString("gongu_id")),rs.getString("category_name"),rs.getString("seller_id"),rs.getString("gongu_name"),
+							rs.getString("gongu_price"),rs.getString("gongu_discount_price"),rs.getString("gongu_status"),rs.getString("gongu_startdate"),
+							rs.getString("gongu_findate"),rs.getString("gongu_reserve"),rs.getString("gongu_min"),rs.getString("thumbnail_img")));
+				} while (rs.next());
+			}	
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return newList;
+	}
+
+	public ArrayList<Gongu> getOldList() {
+		ArrayList<Gongu> oldList = new ArrayList<Gongu>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = con.prepareStatement("select * from gongu where gongu_status = '4' order by gongu_startdate asc limit 4;");
+			
+			rs = pstmt.executeQuery();	
+			
+			if (rs.next()) {
+				do {
+					oldList.add(new Gongu(Integer.parseInt(rs.getString("gongu_id")),rs.getString("category_name"),rs.getString("seller_id"),rs.getString("gongu_name"),
+							rs.getString("gongu_price"),rs.getString("gongu_discount_price"),rs.getString("gongu_status"),rs.getString("gongu_startdate"),
+							rs.getString("gongu_findate"),rs.getString("gongu_reserve"),rs.getString("gongu_min"),rs.getString("thumbnail_img")));
+				} while (rs.next());
+			}	
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return oldList;
+	}
+
 	
 	
 	
